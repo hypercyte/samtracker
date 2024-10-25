cb7082d7e2037fca269def6bc802143960d91aa4
hypercyte <hypercyte@gmail.com>
2024-10-25 15:21:19 +0100

    javescript first draft

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
-    
+
+    // Start date of cycle
+    const startDate = new Date("2024-10-25");
+    const today = new Date(); 
+
+    // Calculating how many days have passed
+    const dayInMillis = 24*60*60*1000;
+    const daysPassed = Math.floor((today - startDate) / dayInMillis);
+
+    // Cycle length (4 days on and 4 days off)
+    const cycleLength = 8;
+
+    // Working day or Free day?
+    const curDay = daysPassed % cycleLength;
+    const isFree = curDay >= 4;
+
+    const statusDiv = document.getElementById("status");
+    statusDiv.textContent = isFree ? "SAM IS FREE TODAY" : "SAM IS NOT FREE TODAY";
+    statusDiv.style.color = isFree ? "green" : "red";
+
 })
